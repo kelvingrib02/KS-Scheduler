8ac1aa6a40d6cede3690e5df00eafdcb5834bfe9
Kelvin Gabriel Ribeiro <58612490+kelvingrib02@users.noreply.github.com>
2026-01-07 00:00:22 -0300

    Ajeitando a home do dashboard

--- src/KS.Scheduler.Frontend/obj/Debug/net8.0/KS.Scheduler.Frontend.AssemblyInfo.cs
+++ src/KS.Scheduler.Frontend/obj/Debug/net8.0/KS.Scheduler.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KS.Scheduler.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baca544c61b19086636a2794492ed93a8957b6e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+317814b5d4ba9e5e195d9c4db726a5372ef2dc0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("KS.Scheduler.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KS.Scheduler.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
