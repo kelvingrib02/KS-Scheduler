87147fd466e848b3a63d188c242009cecc85ae8a
Kelvin Gabriel Ribeiro <58612490+kelvingrib02@users.noreply.github.com>
2026-01-07 15:16:53 -0300

    Nav Menu arrumado

--- src/KS.Scheduler.Frontend/obj/Debug/net8.0/KS.Scheduler.Frontend.AssemblyInfo.cs
+++ src/KS.Scheduler.Frontend/obj/Debug/net8.0/KS.Scheduler.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KS.Scheduler.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ac1aa6a40d6cede3690e5df00eafdcb5834bfe9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+625dec7dbd440fc0de7e91a4b2d247f12f9709ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("KS.Scheduler.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KS.Scheduler.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
