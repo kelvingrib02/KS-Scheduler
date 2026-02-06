b67ded4566891d7ec5ba679a69e81325d45205e4
Kelvin Gabriel Ribeiro <58612490+kelvingrib02@users.noreply.github.com>
2026-02-05 23:53:53 -0300

    Corrigindo Auth

--- src/KS.Scheduler.Application/obj/Debug/net8.0/KS.Scheduler.Application.AssemblyInfo.cs
+++ src/KS.Scheduler.Application/obj/Debug/net8.0/KS.Scheduler.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KS.Scheduler.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e33ac03d7004610ed09a6f37d606a171e48759e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b727f956221274855c3f659b832cbffa018cde88")]
 [assembly: System.Reflection.AssemblyProductAttribute("KS.Scheduler.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KS.Scheduler.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
